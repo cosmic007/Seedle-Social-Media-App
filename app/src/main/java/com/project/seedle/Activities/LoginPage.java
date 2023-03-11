ce54293a597c827261a3c3007aa051c41c598dcf
Abhijith V A <cosmicriderrr@gmail.com>
2023-03-11 23:03:17 +0530

    Loginpage to RegisterActivity

--- app/src/main/java/com/project/seedle/Activities/LoginPage.java
+++ app/src/main/java/com/project/seedle/Activities/LoginPage.java
@@ -2,16 +2,49 @@ package com.project.seedle.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.project.seedle.R;
 
 public class LoginPage extends AppCompatActivity {
+
+    Button loginpagetoReg;
+    Button login;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        loginpagetoReg=findViewById(R.id.LoginPage_RegBtn);
+        login=findViewById(R.id.loginPage_loginbtn);
+
+
+        loginpagetoReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToRegisterPage();
+            }
+        });
+
+
+    }
+
+
+    private void moveToRegisterPage()
+    {
+        try {
+            startActivity(new Intent(this,RegisterActivity.class));
+
+        }
+        catch(Exception e)
+        {
+
+        }
     }
 }
