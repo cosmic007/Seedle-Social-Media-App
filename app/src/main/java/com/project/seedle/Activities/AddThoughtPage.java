b36e8c1e83c1575fa27e95582dd337b2efe19993
shabanamumthaz <127649928+shabanamumthaz@users.noreply.github.com>
2023-03-24 12:45:16 +0530

    AddStatusPage_Update

--- app/src/main/java/com/project/seedle/Activities/AddThoughtPage.java
+++ app/src/main/java/com/project/seedle/Activities/AddThoughtPage.java
@@ -1,16 +1,58 @@
 package com.project.seedle.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.material.tabs.TabLayout;
+import com.project.seedle.AdaptersClasses.AddThoughtsTabAdapter;
+import com.project.seedle.Fragments.addImageThoughtFragment;
+import com.project.seedle.Fragments.addTextThoughtFragment;
 import com.project.seedle.R;
 
 public class AddThoughtPage extends AppCompatActivity {
+
+    private AddThoughtsTabAdapter objectAddThoughtsTabAdapter;
+    private TabLayout objectTabLayout;
+
+    private ViewPager objectViewPager;
+    private int [] tabIcons={
+            R.drawable.icon_text_thoughts,R.drawable.icon_image_thoughts
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_thought_page);
+
+        objectTabLayout =findViewById(R.id.AddThoughts_tabsLayout);
+        objectViewPager=findViewById(R.id.AddThoughts_PageViewer);
+        objectAddThoughtsTabAdapter= new AddThoughtsTabAdapter(getSupportFragmentManager());
+
+        objectAddThoughtsTabAdapter.addFragment(new addTextThoughtFragment(),"Text");
+        objectAddThoughtsTabAdapter.addFragment(new addImageThoughtFragment(),"Image");
+
+        objectViewPager.setAdapter(objectAddThoughtsTabAdapter);
+        objectTabLayout.setupWithViewPager(objectViewPager);
+        SetupIcons();
+
     }
+    private void SetupIcons()
+    {
+        try
+        {
+            objectTabLayout.getTabAt(0).setIcon(tabIcons[0]);
+            objectTabLayout.getTabAt(1).setIcon(tabIcons[1]);
+
+
+        }
+        catch(Exception e)
+        {
+            Toast.makeText(this,"AddThoughts:"+e.getMessage(),Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
--- app/src/main/java/com/project/seedle/AdaptersClasses/AddThoughtsTabAdapter.java
+++ app/src/main/java/com/project/seedle/AdaptersClasses/AddThoughtsTabAdapter.java
@@ -1,6 +1,7 @@
 package com.project.seedle.AdaptersClasses;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
@@ -23,6 +24,7 @@ public class AddThoughtsTabAdapter extends FragmentStatePagerAdapter {
 
 
     }
+    
 
     @NonNull
     @Override
