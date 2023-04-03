b2e9ace8a58439dcf44b948abca5914bb36fd950
Abhijith V A <cosmicriderrr@gmail.com>
2023-04-03 22:03:16 +0530

    update

--- app/src/main/java/com/project/seedle/Activities/TextCommentPage.java
+++ app/src/main/java/com/project/seedle/Activities/TextCommentPage.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
@@ -36,7 +37,7 @@ public class TextCommentPage extends AppCompatActivity {
     //XML Variables
 
     private RecyclerView objectRecyclerView;
-    private Button SendCommentBtn;
+    private TextView SendCommentBtn;
 
     private EditText commentET;
 
--- app/src/main/java/com/project/seedle/Fragments/Community.java
+++ app/src/main/java/com/project/seedle/Fragments/Community.java
@@ -1,14 +1,37 @@
 package com.project.seedle.Fragments;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.project.seedle.R;
+
+
+
+
+
 
 
 public class Community extends Fragment {
@@ -22,7 +45,11 @@ public class Community extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         return inflater.inflate(R.layout.fragment_community, container, false);
+
+
     }
+
+
+
 }
